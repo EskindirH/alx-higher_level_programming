50ca67acccbb8f9b86f2fceb43fe350d38ce116c
EskindirH <eskindirhaileliul@gmail.com>
2023-12-04 06:30:01 -0800

    fixes

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     len_st = len(sentence)
+
     if len_st == 0:
         new_tuple(len_st, None)
     else:
         new_tuple(len_st, sentence[0])
-    return new_tuple    
+
+    return (new_tuple)
