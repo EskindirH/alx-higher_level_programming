5c0045a4aaa1bf0eb9d7f37541c2113ac0a2e602
EskindirH <eskindirhaileliul@gmail.com>
2023-12-19 04:46:04 -0800

    try fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,9 @@ def safe_print_list(my_list=[], x=0):
     for r in range(x):
         try:
             print("{}".format(my_list[r]), end='')
-        except:
+        except IndexError:
             break
         else:
             count += 1
-
     print()
     return (count)
