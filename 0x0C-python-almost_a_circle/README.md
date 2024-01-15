a4feaeccabb1d653415c8a8a8b9a9a1a12efc61e
Eskindir H <eskindirhaileliul@gmail.com>
2024-01-15 06:19:49 -0800

    half done

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,4 @@
+#Python - Almost a circle
+
+All projects are done to learn how to handle class, inheritance, read/write file and json representation of file.
+
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+"""
+
+This module contains a class square
+
+"""
+
+from models.rectangle import Rectangle
+
+class square(Rectangle):
+    """Square class inherits Rectangle
+
+    Args:
+        Rectangle: parent
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes instances
+
+        Args:
+            size: width of square
+            x: point on x
+            y: point on y
+            id: instance id
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Special method
+
+        Returns:
+            stringified value
+        """
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
+
+    @property
+    def size(self):
+        """Getter size
+
+        Returns:
+            size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter size
+
+        Returns:
+            void
+        """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Special method
+
+        Returns:
+            stringified value
+        """
+        str_rectangle = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_size = "{}".format(self.size)
+
+        return str_rectangle + str_id + str_xy + str_size
+
+    def update(self, *args, **kwargs):
+        """Update method
+
+        Args:
+            args: unnamed args
+            kwargs: named args
+
+        Returns:
+            void
+        """
+        if args is not None and len(args) is not 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Dictionary representation of square
+
+        Returns:
+            a dictionary with attributes
+        """
+        list_atr = ['id', 'size', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            if key == 'size':
+                dict_res[key] = getattr(self, 'width')
+            else:
+                dict_res[key] = getattr(self, key)
+
+        return dict_res
